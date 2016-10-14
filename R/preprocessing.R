5d290cf7c24d524f5b350054a78f4bd45ca7be87
Daniel Fürth <daniel.furth@ki.se>
2016-10-14 14:27:18 +0200

    fixed stuff and included imshow()

--- R/preprocessing.R
+++ R/preprocessing.R
@@ -1,20 +1,35 @@
+
+imshow<-function(file=system.file('06_N1_2_2016_10_10_10_10_10.ics', package='fisseq'), channel=1,zstack=1, verbose=FALSE){
+  .Call("imshowICS", file, channel-1, zstack-1, as.integer(verbose))
+
+}
+
 hist.match.ics<-function(file=system.file('06_N1_2_2016_10_10_10_10_10.ics', package='fisseq'), plot=TRUE, verbose=TRUE){
   output<-.Call("readICS", file, as.integer(verbose))
 
-  images<-length(output$histo)/2047
+  if(is.null(output)){return()}
+
+  dynamical.range<-output$range[seq(2,length(output$range), by=2)]-output$range[seq(1,length(output$range), by=2)]
+  images<-length( dynamical.range )
   zstacks<-images/4
-  output.data.tmp<-matrix(output$histo, ncol=images )
+
+  dynamical.range[dynamical.range>2047]<-2048
+
+  index<-c(0, cumsum(t(table(output$imageID))) )
+
+  output.data.tmp<-lapply( seq(1, length(index)-1), function(x){ output$histo[seq(index[x]+1, index[x+1])] } )
 
   output.data<-list()
 
-  intensities<-lapply(seq(1,length(output$range),by=2), function(x){ seq(output$range[x], output$range[x+1], length.out=2047) } )
-  intensities<-do.call("cbind", intensities)
 
-  output.data[[1]]<-list(pixels=output.data.tmp[,1:zstacks], intensity= intensities[,1:zstacks])
-  output.data[[2]]<-list(pixels=output.data.tmp[,1:zstacks+zstacks*1], intensity= intensities[,1:zstacks] )
-  output.data[[3]]<-list(pixels=output.data.tmp[,1:zstacks+zstacks*2], intensity= intensities[,1:zstacks]  )
-  output.data[[4]]<-list(pixels=output.data.tmp[,1:zstacks+zstacks*3], intensity= intensities[,1:zstacks] )
 
+  intensities<-lapply(seq(1,length(output$range),by=2), function(x){ seq(output$range[x], output$range[x+1], length.out=rep(dynamical.range, each=2)[x]-1 ) } )
+  #intensities<-do.call("cbind", intensities)
+
+  output.data[[1]]<-list(pixels=output.data.tmp[1:zstacks], intensity= intensities[1:zstacks])
+  output.data[[2]]<-list(pixels=output.data.tmp[1:zstacks+zstacks*1], intensity= intensities[1:zstacks+zstacks*1] )
+  output.data[[3]]<-list(pixels=output.data.tmp[1:zstacks+zstacks*2], intensity= intensities[1:zstacks+zstacks*2]  )
+  output.data[[4]]<-list(pixels=output.data.tmp[1:zstacks+zstacks*3], intensity= intensities[1:zstacks+zstacks*3] )
 
   if(plot){
 
@@ -26,41 +41,49 @@ hist.match.ics<-function(file=system.file('06_N1_2_2016_10_10_10_10_10.ics', pac
     par(mar=c(4,6,4,6), mfrow=c(4,1))
 
     ecdf.plot<-function(pixels, intensity, ch='Channel 1', color=rgb(0,1,0), transparency=20 ){
-      maximum<-max(cumsum(pixels[,1]))
-      xMax<-ceiling(round( max(intensity[which(round(cumsum(rowMeans(pixels))/maximum,2) == 0.99, arr.ind=TRUE)[1], ])/1000, 1))*1000-950
-      plot(intensity[,1], cumsum(pixels[,1]), type='l', xlim=c(0, xMax), col=paste(color, transparency, sep=''),  ylab='Quantile', xlab='Pixel intensity', axes=FALSE, main=ch)
+      maximum<-max(unlist(lapply(1:length(pixels), function(x){max(cumsum(pixels[[x]]))})))
+      xMax<-max( unlist( lapply(1:zstacks, function(x){ intensity[[x]][which(round(cumsum(pixels[[x]])/maximum,2)==0.99)[1]] }) ), na.rm=TRUE )
+      xMin<-min(unlist(lapply(1:zstacks, function(x){intensity[[x]][which.min(cumsum(pixels[[x]])/maximum)] })), na.rm=TRUE)
+      #<-ceiling(round( max(intensity[which(round(cumsum(rowMeans(pixels))/maximum,2) == 0.99, arr.ind=TRUE)[1], ])/1000, 1))*1000-950
+      plot(intensity[[1]], cumsum(pixels[[1]]), type='l', ylim=c(0,maximum), xlim=c(xMin, xMax), col=paste(color, transparency, sep=''),  ylab='Quantile', xlab='Pixel intensity', axes=FALSE, main=ch)
       axis(1)
       axis(2, at=c(0,maximum*0.5,maximum), labels=c(0,0.5,1), las=1)
       axis(4, at=c(0,maximum*0.5,maximum), labels=c(0,round(maximum*0.5/10^6, 2), round(maximum/10^6, 2)), las=1)
       mtext('Mega pixels', 4, 3, cex=0.7 )
 
-      lapply(1:zstacks, function(x){lines(intensity[,x], cumsum(pixels[,x]), col=paste(color, transparency, sep='') ) })
+      lapply(1:zstacks, function(x){lines(intensity[[x]], cumsum(pixels[[x]]), col=paste(color, transparency, sep='') ) })
 
-      fit<-smooth.spline(intensity[,1], cumsum(pixels[,1]))
-      smoothed<-predict(fit, seq(0,xMax))$y
+      if(length(intensity[[1]])>3){
+        fit<-smooth.spline(intensity[[1]], cumsum(pixels[[1]]))
+        smoothed<-predict(fit, seq(xMin,xMax))$y
+      }else{
+        smoothed<-numeric()
+      }
       for(i in 1:zstacks){
-        fit<-smooth.spline(intensity[,i], cumsum(pixels[,i]))
-        smoothed<-rbind(smoothed, predict(fit, seq(0,xMax))$y )
+        if(length(intensity[[i]])>3){
+          fit<-smooth.spline(intensity[[i]], cumsum(pixels[[i]]))
+          smoothed<-rbind(smoothed, predict(fit, seq(xMin,xMax))$y )
+        }
       }
 
       quant<-apply(smoothed, 2, function(x){quantile(x,0.5)})
 
 
 
-      lines(quant, col='black', lwd=3 )
-      lines(quant, col=color, lwd=1.5 )
+      lines(seq(xMin,xMax),quant, col='black', lwd=3 )
+      lines(seq(xMin,xMax),quant, col=color, lwd=1.5 )
 
       ED50<-which.min(abs(quant-maximum/2))
 
-      lines(c(-1000 , ED50), rep(quant[ED50],2))
-      lines(rep(ED50,2), c(-1000, quant[ED50]))
-      points(ED50, quant[ED50], pch=21, bg='orange')
+      lines(c(-1000 , seq(xMin,xMax)[ED50]), rep(quant[ED50],2))
+      lines(seq(xMin,xMax)[rep(ED50,2)], c(-1000, quant[ED50]))
+      points(seq(xMin,xMax)[ED50], quant[ED50], pch=21, bg='orange')
       abline(h=c(-1000, maximum), lty=2, col='darkgray')
 
-      ED95<-which.min(abs(quant-maximum*0.95))
-      ED05<-which.min(abs(quant-maximum*0.05))
+      ED95<-seq(xMin,xMax)[which.min(abs(quant-maximum*0.95))]
+      ED05<-seq(xMin,xMax)[which.min(abs(quant-maximum*0.05))]
 
-      legend('bottomright', c(paste('Median = ',ED50), paste('95% quantile = ',ED95), paste('5% quantile = ',ED05)), pch=c(21,NA,NA), pt.bg='orange', box.lty = 0)
+      legend('bottomright', c(paste('Median = ', seq(xMin,xMax)[ED50]), paste('95% quantile = ',ED95), paste('5% quantile = ',ED05)), pch=c(21,NA,NA), pt.bg='orange', box.lty = 0)
     }
 
     ecdf.plot(output.data[[1]]$pixels, output.data[[1]]$intensity, ch='Channel 1', color=rgb(0,1,1), transparency=30)
--- src/readICS.cpp
+++ src/readICS.cpp
@@ -17,6 +17,8 @@ using namespace cv;
 #include <unistd.h>
 #include <string>
 #include <stdint.h>
+#include <math.h>       /* pow */
+
 
 using namespace std;
 
@@ -86,6 +88,7 @@ RcppExport SEXP readICS(SEXP input, SEXP verbose) {
 
   if(VERBOSE){
     Rcpp::Rcout << "Data type: " << getImgTypes(dt) << std::endl;
+    IcsPrintIcs(ip);
   }
 
   bufsize = IcsGetDataSize (ip);
@@ -126,6 +129,8 @@ RcppExport SEXP readICS(SEXP input, SEXP verbose) {
 
     std::vector< float > histoCount;
     std::vector<float> rangeR;
+    std::vector<int> imageID;
+    int imageCounter = 1;
     for(int col=0; col< (int)ip->Dim[4].Size;col++){
     //loop through z-stacks
    for(int k=0; k< (int)ip->Dim[2].Size;k++){
@@ -142,21 +147,47 @@ RcppExport SEXP readICS(SEXP input, SEXP verbose) {
 
     double hmin, hmax;
     minMaxLoc(MatrixName, &hmin, &hmax);
-    //Rcpp::Rcout <<  "Max, Min: " << hmin << " | " << hmax << std::endl;
-
     int histSize = 2048;
-    float range[] = {hmin, hmax};
-    const float* histRange = { range };
-    bool uniform = true; bool accumulate = false;
+    if( (histSize>=(hmax-hmin)) ){
+      histSize = (int)(hmax-hmin);
+    }
 
-    Mat hist;
-    calcHist( &MatrixName, 1, 0, Mat(), hist, 1, &histSize, &histRange, uniform, accumulate);
+    float range[] = {hmin, hmax};
+    /* if(imageCounter==36 | imageCounter==57){
+    Rcpp::Rcout << "Max, Min: " << hmax << hmin << std::endl;
+    } */
+
+    if( histSize>1 ){
+
+      const float* histRange = { range };
+      bool uniform = true; bool accumulate = false;
+
+      Mat hist;
+      calcHist( &MatrixName, 1, 0, Mat(), hist, 1, &histSize, &histRange, uniform, accumulate);
 
 
     //Mat canvas = Mat::zeros(200, histSize, CV_8UC1);
 
-    for (int j = 0; j < histSize-1; j++){
-      histoCount.push_back(hist.at<float>(j));
+      for (int j = 0; j < histSize-1; j++){
+        histoCount.push_back(hist.at<float>(j));
+        imageID.push_back(imageCounter);
+      }
+    }else{
+      range[0] = ((hmin-10) > 0) ? hmin-10 : 0;
+      range[1] = ((hmax+10) < pow(2,16)) ? hmax+10 : pow(2,16);
+      histSize =  range[1]-range[0];
+
+      const float* histRange = { range };
+      bool uniform = true; bool accumulate = false;
+
+      Mat hist;
+      calcHist( &MatrixName, 1, 0, Mat(), hist, 1, &histSize, &histRange, uniform, accumulate);
+
+      for (int j = 0; j < histSize-1; j++){
+        histoCount.push_back(hist.at<float>(j));
+        imageID.push_back(imageCounter);
+
+      }
     }
 
     /* MatrixName.convertTo(MatrixName,CV_8UC1,255.0/(200-0));
@@ -165,6 +196,7 @@ RcppExport SEXP readICS(SEXP input, SEXP verbose) {
     destroyWindow("test"); */
     rangeR.push_back(range[0]);
     rangeR.push_back(range[1]);
+    imageCounter++;
    }
    if(VERBOSE){Rcpp::Rcout << "  [";
      int pos = barWidth * progress;
@@ -181,9 +213,7 @@ RcppExport SEXP readICS(SEXP input, SEXP verbose) {
    }
     }
 
-    if(VERBOSE){
-  IcsPrintIcs(ip);
-    }
+
 
   retval = IcsClose (ip);
   if (retval != IcsErr_Ok){}
@@ -192,8 +222,162 @@ RcppExport SEXP readICS(SEXP input, SEXP verbose) {
 
   return List::create(
     _["histo"] = histoCount,
-    _["range"] = rangeR
+    _["range"] = rangeR,
+    _["imageID"] = imageID
   );
+  }
+  END_RCPP
+}
+
+
+
+
+
+RcppExport SEXP imshowICS(SEXP input, SEXP chToShow, SEXP zsToShow, SEXP verbose) {
+  BEGIN_RCPP
+  Rcpp::RNGScope __rngScope;
+
+  Rcpp::CharacterVector f(input);
+  std::string ff(f[0]);
+  //convert to C char version
+  const char *filenameChar = ff.c_str();
+
+  int VERBOSE = Rcpp::as<int>(verbose);
+  int channelToShow = Rcpp::as<int>(chToShow);
+  int zstackToShow = Rcpp::as<int>(zsToShow);
+
+  ICS* ip;
+  Ics_DataType dt;
+  int ndims;
+  size_t dims[ICS_MAXDIM];
+  size_t strides[ICS_MAXDIM];
+  size_t bufsize;
+  void* buf;
+  Ics_Error retval;
+  char filename[ICS_MAXPATHLEN];
+  int elemsize;
+  int ii;
+  size_t tmp;
+  char errormessage[2048];
+
+  int barWidth = 70;
+  float progress = 0.0;
+
+  ///Users/danielfurth/Downloads/fisseq_test.ics
+  retval = IcsOpen (&ip, filenameChar, "r");
+  if (retval != IcsErr_Ok){
+    Rcpp::Rcout << "ERROR: " << "could not open file. Check format." << std::endl;
+    IcsGetLayout (ip, &dt, &ndims, dims);
+
+    if(VERBOSE){
+      Rcpp::Rcout << "Data type: " << getImgTypes(dt) << std::endl;
+    }
+    return R_NilValue;
+  }else{
+
+
+    IcsGetLayout (ip, &dt, &ndims, dims);
+
+    if(VERBOSE){
+      Rcpp::Rcout << "Data type: " << getImgTypes(dt) << std::endl;
+      IcsPrintIcs(ip);
+    }
+
+    bufsize = IcsGetDataSize (ip);
+    buf = malloc (bufsize);
+    if (buf == NULL){
+      Rcpp::Rcout << "====== ERROR ======" << std::endl;
+      Rcpp::Rcout << "Could not allocate memory" << std::endl;
+
+      return R_NilValue;
+    }
+    /* Flag error condition*/ ;
+    retval = IcsGetData (ip, buf, bufsize);
+    if (retval != IcsErr_Ok){
+      Rcpp::Rcout << "====== ERROR ======" << std::endl;
+      Rcpp::Rcout << "Could not open data" << std::endl;
+
+      return R_NilValue;
+    }
+    if(VERBOSE){ Rcpp::Rcout << "====== Image read ======" << std::endl;}
+
+    int p;
+    Ics_Format Format;
+    int Sign;
+    size_t Bits;
+    char* s;
+
+    IcsGetPropsDataType (ip->Imel.DataType, &Format, &Sign, &Bits);
+
+    p = ip->Dimensions;
+    int elements = 1;
+    for (ii=0; ii<p; ii++)
+      elements = elements*(unsigned long) ip->Dim[ii].Size;
+
+    std::vector<uint16_t> b(elements);
+    for(int j=0;j<elements;j++){
+      b[j] = ((uint16_t *)buf)[j];
+    }
+
+    for(int col=0; col< (int)ip->Dim[4].Size;col++){
+      //loop through z-stacks
+      for(int k=0; k< (int)ip->Dim[2].Size;k++){
+
+
+
+        Mat MatrixName = Mat((int)ip->Dim[1].Size, (int)ip->Dim[0].Size, CV_16U);
+
+        for(int i=0; i< (int)ip->Dim[1].Size;i++){
+          for(int j=0;j<(int)ip->Dim[0].Size;j++){
+            MatrixName.at<ushort>(i,j) = (int)b[i*(int)ip->Dim[0].Size + j + k*((int)ip->Dim[0].Size*(int)ip->Dim[1].Size) + col*((int)ip->Dim[0].Size*(int)ip->Dim[1].Size*(int)ip->Dim[2].Size) ];
+          }
+        }
+
+      if(col== channelToShow && k == zstackToShow){
+        double hmin, hmax;
+        Mat dsp;
+        dsp = MatrixName.clone();
+        if(dsp.rows>600){
+          double ScaleFactor;
+          ScaleFactor = 600/(double)dsp.rows;
+          resize(dsp, dsp, Size(), ScaleFactor, ScaleFactor, INTER_LINEAR);
+        }
+        minMaxLoc(dsp, &hmin, &hmax);
+        dsp -= hmin;
+        dsp.convertTo(dsp,CV_8UC1,255.0/(hmax-hmin));
+        imshow("display", dsp);
+        Rcpp::Rcout << "Max value: "<<  hmax << std::endl;
+        Rcpp::Rcout << "Min value: "<<  hmin << std::endl;
+        Rcpp::Rcout << "CLICK ON DISPLAY AND PRESS ESC TO CLOSE IMAGE" << std::endl;
+
+        waitKey(0);
+        destroyWindow("display");
+      }
+
+    }
+      if(VERBOSE){Rcpp::Rcout << "  [";
+        int pos = barWidth * progress;
+        for (int w = 0; w < barWidth; ++w) {
+          if (w < pos) Rcpp::Rcout << "=";
+          else if (w == pos) Rcpp::Rcout << ">";
+          else Rcpp::Rcout << " ";
+        }
+        Rcpp::Rcout << "] " << int(progress * 100.0) << "% \r" << std::cout.flush();//cout.flush();
+        R_FlushConsole();
+        R_ProcessEvents();
+        R_CheckUserInterrupt();
+        progress += (float)1/((int)ip->Dim[4].Size-1);
+      }
+    }
+
+
+
+    retval = IcsClose (ip);
+    if (retval != IcsErr_Ok){}
+    /* Flag error condition*/ ;
+
+
+    return R_NilValue;
   }
   END_RCPP
 }
